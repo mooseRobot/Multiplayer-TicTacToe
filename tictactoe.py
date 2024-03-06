cf1f7623d226f166ea93ecb46f8b78e7d4fc649a
Felix <111679444+mooseRobot@users.noreply.github.com>
2024-03-05 20:48:34 -1000

    Rework random player piece

--- tictactoe.py
+++ tictactoe.py
@@ -2,12 +2,17 @@ import random
 class TicTacToe():
     
     def __init__(self, players: list):
-        self.x = players.pop(random.choice(players))
+        self.x = players.pop(random.randint(0,1))
         self.o = players[0]
         self.currenturn = self.o
         self.board = [["" for x in range(3)] for i in range(3)]
 
     def print_board(self):
+        """Returns a string of the current board
+
+        Returns:
+            str: String of the current boaard
+        """
         board_str = ""
         for i in range(3):
             for j in range(3):
@@ -18,3 +23,20 @@ class TicTacToe():
             if i < 2:
                 board_str += "-" * 5 + "\n"
         return board_str
+    
+    def get_x(self):
+        return self.x
+    
+    def get_o(self):
+        return self.o
+    
+    def get_current_turn(self):
+        return self.currenturn
+    
+    def change_turn(self):
+        if self.currenturn == self.o:
+            self.currenturn = self.x
+        else:
+            self.currenturn = self.o
+
+
